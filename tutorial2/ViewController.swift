740ecb3c606a87032256e9fe9dbc4bb1448d6d0d
barteaga <barteaga@umich.edu>
2020-04-25 13:54:58 -0400

    Initial Commit

---
+++ tutorial2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tutorial2
+//
+//  Created by Ben Arteaga on 4/25/20.
+//  Copyright © 2020 Ben Arteaga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
